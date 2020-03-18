8d798a7991e666c666bdbe1a094bc88270b0fddc
SDI-SRL <62357101+SDI-SRL@users.noreply.github.com>
2020-03-18 17:11:09 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charser="UTF-8">
+    <title>pagina de prueba para TIS</title>
+</head>
+<body>
+    <h1>SDI SRL</h1>
+    <h2>PAGINA DE PRUEBA</h2>
+</body>
+</html>
